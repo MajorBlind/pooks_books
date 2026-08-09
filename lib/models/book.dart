8a7b027288261778b954856e77921c419b2b4a33
MajorBlind <alan.opac16@gmail.com>
2026-08-09 03:12:01 -0400

    Add methods to translate between Book object and Map

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -28,4 +28,42 @@ class Book{
     this.summary,
     this.coverPath,
   });
+
+  Map<String, dynamic> toMap(){
+    return{
+      'id': id,
+      'title': title,
+      'author': author,
+      'series': series,
+      'status': status.name,
+      'startDate': startDate?.toIso8601String(),
+      'finishDate': finishDate?.toIso8601String(),
+      'rating': rating,
+      'spiceRating': spiceRating,
+      'notes': notes,
+      'summary': summary,
+      'coverPath': coverPath,
+    };
+  }
+
+  factory Book.fromMap(Map<String, dynamic> map){
+    return Book(
+      id: map['id'] as int?,
+      title: map['title'] as String,
+      author: map['author'] as String,
+      series: map['series'] as String?,
+      status: ReadingStatus.values.byName(map['status'] as String),
+      startDate: map['startDate'] != null
+        ? DateTime.parse(map['startDate'] as String)
+        : null,
+      finishDate: map['finishDate'] != null
+        ? DateTime.parse(map['finishDate'] as String)
+        : null,
+      rating: map['rating'] as double?,
+      spiceRating: map['spiceRating'] as int?,
+      notes: map['notes'] as String?,
+      summary: map['summary'] as String?,
+      coverPath: map['coverPath'] as String?,
+    );
+  }
 }
